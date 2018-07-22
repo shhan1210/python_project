1dbf91b436e77f84eba76615312cd97631e6bfa9
Han Seung Hun <shhan1210@gmail.com>
2018-07-22 14:37:39 +0900

    Add 02-data-type-1.py

---
+++ 02-data-type-1.py
@@ -0,0 +1,14 @@
+#정수 int
+print(1 + 1)
+#소수 float
+print(1.5 + 2.1)
+#문자 string
+print("hello")
+
+# print("hello" + "1")
+# print(4 // 2)
+# print(int(4 / 2))
+# print(float(2))
+print("1")
+print(int("1"))
+print(int("1.5"))
